0e5b57e787b81bf21cd202c96fb2d531fbfb0dce
Farzad_Sarfaraz <129787719+Farzadsarfaraz@users.noreply.github.com>
2024-07-25 17:16:06 +0200

    id logic was added

--- index.js
+++ index.js
@@ -79,8 +79,18 @@ const Product = mongoose.model("Product", {   // Product is the name of the Sche
 })
 
 app.post('/addproduct', async (req, res)=>{ // we use our Schema here
+    let products = await Product.find({})  // with this we can get all the product in one array
+    let id;
+    if(products.length >0)
+    {
+        let last_product_array = product.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id+1;
+    }else{
+        id = 1
+    }
     const product = new Product({
-        id:req.body.id,
+        id:id,         
         name:req.body.name,
         image:req.body.image,
         category:req.body.category,
